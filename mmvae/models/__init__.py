368c2c9a601b63c1e526d715da71a94e81b7eff0
Gabriela Šejnová <sejnogab@ciirc.cvut.cz>
2021-06-29 12:33:08 +0200

    mmvae initial commit

---
+++ mmvae/models/__init__.py
@@ -0,0 +1,15 @@
+from .mmvae_cub_images_sentences import CUB_Image_Sentence as VAE_cubIS
+from .mmvae_cub_images_sentences_ft import CUB_Image_Sentence_ft as VAE_cubISft
+from .mmvae_own import MOE as VAE_2mods
+from .mmvae_own import POE as VAE_2mods_poe
+from .vae_cub_image import CUB_Image as VAE_cubI
+from .vae_cub_image_ft import CUB_Image_ft as VAE_cubIft
+from .vae_cub_sent import CUB_Sentence as VAE_cubS
+from .vae_mnist import MNIST as VAE_mnist
+from .vae_mnist import CROW as VAE_imgcol
+from .vae_svhn import SVHN as VAE_svhn
+from .vae_svhn import CROW2 as VAE_imgtxt
+from .vae_own import UNIVAE as VAE_uni
+
+__all__ = [VAE_2mods, VAE_mnist, VAE_svhn, VAE_cubIS, VAE_cubS,
+           VAE_cubI, VAE_cubISft, VAE_cubIft, VAE_imgcol, VAE_imgtxt, VAE_uni, VAE_2mods_poe]
