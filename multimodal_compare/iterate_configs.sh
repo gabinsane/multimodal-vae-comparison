49a73451fde33711190b260591e328b52f438f85
Gabriela Šejnová <g.sejnova@gmail.com>
2022-08-15 22:43:58 +0200

    config iterator

--- multimodal_compare/iterate_configs.sh
+++ multimodal_compare/iterate_configs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 FILES=$@
+COUNTER=0
 cd ~/multimodal-vae-comparison/multimodal_compare
-allfiles= find $FILES -name "*.yml"
+allfiles=$(find $FILES -name "*.yml")
 for f in $allfiles;
-  do echo "Config $f";
-  python main.py --cfg "$f"
+do
+  COUNTER=$(( COUNTER + 1 ));
+  echo "Config $f";
+  python main.py --cfg "$f";
 done
+echo "Finished all $COUNTER experiments"
