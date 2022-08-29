1b3ff8aa0e3805041bcae9a785cf061ef110c1ed
Jan Behrens <jan.kristof.behrens@cvut.cz>
2022-08-29 14:12:24 +0200

    making enum for Network roles

--- multimodal_compare/models/NetworkTypes.py
+++ multimodal_compare/models/NetworkTypes.py
@@ -1,6 +1,15 @@
 from enum import Enum
 
+
 class NetworkTypes(Enum):
+    UNSPECIFIED = -1
     CNN = 1
     TXTTRANSFORMER = 2
     FNN = 3
+
+
+class NetworkRoles(Enum):
+    UNSPECIFIED = -1
+    ENCODER = 1
+    DECODER = 2
+    MIXER = 3
