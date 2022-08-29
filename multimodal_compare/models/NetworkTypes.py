430702f5374f30ac5a563cd8d6910c75f7784aa0
Gabriela Šejnová <g.sejnova@gmail.com>
2022-08-29 12:26:38 +0200

    base vae and mmvae

--- multimodal_compare/models/NetworkTypes.py
+++ multimodal_compare/models/NetworkTypes.py
@@ -1,6 +1,9 @@
 from enum import Enum
+from collections import namedtuple
 
 class NetworkTypes(Enum):
     CNN = 1
     TXTTRANSFORMER = 2
     FNN = 3
+
+VaeOutput = namedtuple("vae_output", ["encoder_dists", "decoder_dists","latent_samples"])
