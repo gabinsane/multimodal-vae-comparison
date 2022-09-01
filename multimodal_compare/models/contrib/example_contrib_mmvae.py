a1ba74fbcdb156cb264868cfebd5e4b07cdf250a
Gabriela Šejnová <g.sejnova@gmail.com>
2022-09-01 17:47:19 +0200

    minimal model exmaple

--- multimodal_compare/models/contrib/example_contrib_mmvae.py
+++ multimodal_compare/models/contrib/example_contrib_mmvae.py
@@ -1,102 +1,54 @@
 from ..mmvae_base import TorchMMVAE
-import torch.distributions as dist
+import torch
+from torch.autograd import Variable
 
-
-class ExampleTorchVAE(TorchMMVAE):
-    def __init__(self):
-        super(ExampleTorchVAE).__init__()
-
-    def forward(self, inputs, K=1):
-        pass
-
-
-class MOE(TorchMMVAE):
-    modelname = 'moe'
-
-    def __init__(self, encoders, decoders, data_paths, feature_dims, mod_types, n_latents, test_split, batch_size):
+class POE(TorchMMVAE):
+    modelname = 'poe'
+    def __init__(self, vaes, model_config=None):
         """
         Multimodal Variaional Autoencoder with Mixture of Experts https://github.com/iffsid/mmvae
-
-        :param encoders: list of encoder names (strings) as listed in config
+        :param vaes: list of modality-specific vae objects
         :type encoders: list
-        :param decoders: list of decoder names (strings) as listed in config
-        :type decoders: list
-        :param data_paths: list of data paths for all modalities
-        :type data_paths: list
-        :param feature_dims: list of modality-specific feature dimensions as listed in config
-        :type feature_dims: list
-        :param mod_types: list of modality types (strings) from config
-        :type mod_types: list
-        :param n_latents: list of latent dimensionalities from config
-        :type n_latents: list
-        :param test_split: fraction of the data to be used for validation
-        :type test_split: float
-        :param batch_size: batch size
-        :type batch_size: int
+        :param model_cofig: config with model-specific parameters
+        :type model_config: dict
         """
-        self.modelName = 'moe'
-        super(MOE, self).__init__(dist.Normal, encoders, decoders, data_paths, feature_dims, mod_types, n_latents,
-                                  test_split, batch_size)
+        super().__init__()
+        self.vaes = torch.nn.ModuleDict(vaes)
+        self.model_config = model_config
+        self.modelName = 'poe'
 
-    def get_missing_modalities(self, mods):
-        """
-        Get indices of modalities that are missing
+    def get_batch_size(self, inp):
+        """Estimates batch size from the input"""
+        batch_size = None
+        for key in inp.keys():
+            if inp[key] is not None:
+                batch_size = inp[key][0].shape[0]
+                break
+        return batch_size
 
-        :param mods: list of modalities
-        :type mods: list
-        :return: list of indices of missing modalities
-        :rtype: list
-        """
-        indices = []
-        for i, e in enumerate(mods):
-            if e is None:
-                indices.append(i)
-        return indices
+    def modality_mixing(self, mods):
+        """ Calculate the product of experts to get the joint posterior """
+        batch_size = self.get_batch_size(mods)
+        mu, logvar = self.prior_expert((1, batch_size, self.vaes["mod_1"].n_latents))
+        for m, params in mods.items():
+            if params is not None:
+                mu = torch.cat((mu, params[0].unsqueeze(0)), dim=0)
+                logvar = torch.cat((logvar, params[1].unsqueeze(0)), dim=0)
+        eps = 1e-8
+        var = torch.exp(logvar) + eps
+        T = 1. / var
+        pd_mu = torch.sum(mu * T, dim=0) / torch.sum(T, dim=0)
+        pd_var = 1. / torch.sum(T, dim=0)
+        for key in mods.keys():
+            mods[key] = [pd_mu, pd_var]
+        return mods
 
-    def forward(self, x, K=1):
+    def prior_expert(self, size):
+        """Universal prior expert. Here we use a spherical
+        Gaussian: N(0, 1).
+        @param size: integer
+                     dimensionality of Gaussian
         """
-        Forward pass that takes input data and outputs a list of posteriors, reconstructions and latent samples
-
-        :param x: input data, a list of modalities where missing modalities are replaced with None
-        :type x: list
-        :param K: sample K samples from the posterior
-        :type K: int
-        :return: a list of posterior distributions, a list of reconstructions and latent samples
-        :rtype: tuple(list, list, list)
-        """
-        qz_xs, zss = [], []
-        # initialise cross-modal matrix
-        px_zs = [[None for _ in range(len(self.vaes))] for _ in range(len(self.vaes))]
-        for m, vae in enumerate(self.vaes):
-            if x[m] is not None:
-                qz_x, px_z, zs = vae(x[m], K=K)
-                qz_xs.append(qz_x)
-                zss.append(zs)
-                px_zs[m][m] = px_z  # fill-in diagonal
-        for ind in self.get_missing_modalities(x):
-            lat = zss[0] if "transformer" not in self.vaes[ind].dec_name.lower() else [zss[0], None]
-            px_zs[ind][ind] = self.vaes[ind].px_z(*self.vaes[ind].dec(lat))
-        for e, zs in enumerate(zss):
-            for d, vae in enumerate(self.vaes):
-                if e != d:  # fill-in off-diagonal
-                    if "transformer" in self.vaes[d].dec_name.lower():
-                        px_zs[e][d] = vae.px_z(*vae.dec([zs, x[d][1]] if x[d] is not None else [zs, None]))
-                    else:
-                        px_zs[e][d] = vae.px_z(*vae.dec(zs))
-        return qz_xs, px_zs, zss
-
-    def reconstruct(self, data, runPath, epoch, N=8):
-        """
-        Reconstruct data for individual experts
-
-        :param data: list of input modalities
-        :type data: list
-        :param runPath: path to save data to
-        :type runPath: str
-        :param epoch: current epoch to name the data
-        :type epoch: str
-        :param N: how many samples to reconstruct
-        :type N: int
-        """
-        recons_mat = super(MOE, self).reconstruct([d for d in data])
-        self.process_reconstructions(recons_mat, data, epoch, runPath)
+        mu = Variable(torch.zeros(size)).to("cuda")
+        logvar = Variable(torch.log(torch.ones(size))).to("cuda")
+        return mu, logvar
