d27d5fb899aa2481fd32cd4fc9724a4c1622b9be
Gabriela Šejnová <g.sejnova@gmail.com>
2023-06-21 16:21:17 +0200

    configs

--- multimodal_compare/data_proc/generate_configs.py
+++ multimodal_compare/data_proc/generate_configs.py
@@ -14,7 +14,7 @@ def exclude_keys(d, keys):
     return {x: d[x] for x in d if x not in keys}
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--path", type=str, default="./configs/dmvae", help="Folder in which to save the configs")
+parser.add_argument("-p", "--path", type=str, default="./configs/reproduce_paper/mopoe", help="Folder in which to save the configs")
 parser.add_argument('--exp-name', type=str, default="level5",
                     help='name of the config file')
 parser.add_argument('--cfg', type=str, default="./configs/config1.yml",
